17d73564a769ca7f02668f4c319f88c21bf042ed
Thiago Mota <thiagomotape@hotmail.com.br>
2024-01-10 18:15:02 -0300

    mini game
    converti um mini game web pra desktop

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const reloadButtom = document.querySelector('.reload-buttom');
+
+const marioJump = () => {
+    mario.classList.add('mario-jump');
+    
+    setTimeout(() => {
+        mario.classList.remove('mario-jump');
+    }, 500)
+}
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if(pipePosition < 115 && pipePosition > 0 && marioPosition <=  70) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+        mario.src = '../assets/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '38px';
+
+        reloadButtom.style.display = 'initial';
+
+        clearInterval(loop);
+    }
+}, 10);
+
+document.addEventListener('keydown', marioJump);
